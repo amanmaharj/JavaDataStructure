42045ae1266a2ee2c9cff5f28795c2b7236b2a9b
amanmaharj <aman.mahaj@gmail.com>
2023-07-02 03:04:20 -0600

    RadixSort

---
+++ RadixSort.java
@@ -0,0 +1,8 @@
+public class RadixSort {
+    static int[] radixArray = {4725, 4586, 1330, 8792, 1594, 5729};
+    public static void main(String[] args) {
+        for(int i = 0; i<radixArray.length; i++){
+            System.out.println(radixArray[i]);
+        }
+    }
+}
